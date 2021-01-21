9b9c20eb4b8784a8ff62a9cfc4db108562015ad2
TheDutchMC <github@thedutchmc.nl>
2021-01-21 18:14:24 +0100

    Cleanup of remote. See previous commit

--- src/main/java/nl/thedutchmc/betterplayer/commands/CommandExecutor.java
+++
@@ -1,8 +0,0 @@
-package nl.thedutchmc.betterplayer.commands;
-
-import nl.thedutchmc.betterplayer.BetterPlayer;
-
-public interface CommandExecutor {
-	
-	public void fireCommand(BetterPlayer betterPlayer, CommandParameters parameters);
-}
